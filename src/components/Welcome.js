385b58dd267675bf3ebaa28f2b4f90bc1e9d4945
Zara Siddique <zara.siddique@amplyfigroup.com>
2024-08-27 15:44:57 +0100

    Links to external orgs added

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -22,7 +22,7 @@ function Welcome() {
             alignItems: "center", // Centers the image vertically
           }}
         >
-          <Col md={{ span: 5, offset: 1}} className="text-end">
+          <Col md={{ span: 5, offset: 1 }} className="text-end">
             <p>
               Nazmia and Zara set up Lez Read in March 2022 as a way to find
               queer friends in Cardiff without going to bars and nightclubs. Our
@@ -38,6 +38,14 @@ function Welcome() {
               place, along with an archive of the zines and posters for these
               events.
             </p>
+            <p>
+              We are grateful to the{" "}
+              <a href="https://llyfrau.cymru/en/" target="_blank">
+                Books Council of Wales
+              </a>{" "}
+              for funding that has helped us to create this website and fund
+              other Lez Read activties/resources.
+            </p>
           </Col>
           <Col md={{ span: 4, offset: 1 }}>
             <Image
@@ -47,6 +55,48 @@ function Welcome() {
               src={zara_and_naz}
               fluid
             />
+          </Col>
+        </Row>
+        <h2 className="text-center pt-5">Our Partners and Friends</h2>
+        <Row className="pt-4 pb-5">
+          <Col md={4} className="text-center mb-4">
+            <a
+              href="https://thequeeremporium.co.uk"
+              target="_blank"
+            >
+              <img
+                src={require("../assets/logos/tqe-logo.png")}
+                alt="The Queer Emporium"
+                width="150"
+                height="auto"
+              />
+            </a>
+          </Col>
+          <Col md={4} className="text-center mb-4">
+            <a
+              href="https://www.instagram.com/lezwritecardiff"
+              target="_blank"
+            >
+              <img
+                src={require("../assets/logos/lezwrite.png")}
+                alt="Lez Write"
+                width="150"
+                height="auto"
+              />
+            </a>
+          </Col>
+          <Col md={4} className="text-center mb-4">
+            <a
+              href="https://paned-o-ge.wales/"
+              target="_blank"
+            >
+              <img
+                src={require("../assets/logos/paned.png")}
+                alt="Paned o Ge"
+                width="150"
+                height="auto"
+              />
+            </a>
           </Col>
         </Row>
       </Container>
